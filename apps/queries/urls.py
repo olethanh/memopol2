bc5ecd39c24ae95d95a43e6591a10e82adddba0c
Stefan Praszalowicz <stefan@popidiou.net>
2011-03-04 02:06:04 +0100

    queries: move template to template/queries, and add a simple view+template for sql indexer based queries

--- apps/queries/urls.py
+++ apps/queries/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls.defaults import patterns, url
 
-import views
+from queries import views
 
 urlpatterns = patterns('',
     url(r'^$', views.query, name='query'),
+    url(r'^indexq$', views.indexquery, name='indexquery'),
 )
--- apps/queries/views.py
+++ apps/queries/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render_to_response
 from django.views.generic.simple import direct_to_template
 
 from meps.models import MEP
+from indexer.models import MEPDescriptor
 from forms import QueryForm
 
 def query(request):
@@ -19,4 +20,18 @@ def query(request):
          form.cleaned_data.get('commitee_filter',None)]
     print key
     meps = MEP.view('meps/query', key=key)
-    return render_to_response('query.html', { 'meps': meps})
+    return render_to_response('queries/query.html', { 'meps': meps})
+
+def indexquery(request):
+    form = QueryForm(request.GET)
+    if not form.is_valid():
+        return render_to_response('queries/iquery.html')
+    options = {}
+    country = form.cleaned_data.get('country_filter', None)
+    group = form.cleaned_data.get('group_filter', None)
+    if country:
+        options['country'] = country
+    if group:
+        options['group'] = group
+    meps = MEPDescriptor.objects.filter(**options)
+    return render_to_response('queries/iquery.html', { 'meps': meps})
