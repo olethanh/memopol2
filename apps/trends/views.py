e1ac786e7a03517c062c942962f691ac0b7ab2c6
Olivier Le Thanh Duong <olivier@lethanh.be>
2011-04-03 00:02:38 +0200

    [enh] Trends : add an option to force generation
    Add a get parameter 'force', to force the view to regenerate the image

--- apps/trends/views.py
+++ apps/trends/views.py
@@ -1,6 +1,7 @@
 import logging
 import os
 from os.path import realpath
+from datetime import datetime
 
 from django.conf import settings
 from django.http import HttpResponse, HttpResponseNotFound
@@ -31,11 +32,14 @@ def send_file(request, filename, content_type='text/plain'):
 
 def trends_for_mep(request, mep_id):
     filename = realpath(".%simg/trends/meps/%s-scores.png" % (settings.MEDIA_URL, mep_id))
-    if not os.path.exists(filename):
+    force = request.GET.get(u'force', '0')
+    force = False if force == '0' else True
+
+    if force or not os.path.exists(filename):
         mep_ = MEP.get(mep_id)
         score_list = mep_.scores
+        score_list.sort(key = lambda k : datetime.strptime(k['date'], "%d/%m/%Y"))
         scores = [s['value'] for s in mep_.scores]
-
         if scores and has_matplotlib:
                 pyplot.plot(scores, 'bo')
                 a, b = numpy.polyfit(range(len(scores)), [int(x) for x in scores], 1)
