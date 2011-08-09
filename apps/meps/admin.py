463596d5c01a41f7d75b85342b64b657568383b3
Olivier Le Thanh Duong <olivier@lethanh.be>
2011-08-10 00:58:09 +0200

    [enh]add Organization{,Mep} in admin and as inline of Mep

--- apps/meps/admin.py
+++ apps/meps/admin.py
@@ -1,6 +1,6 @@
 from reps.models import WebSite, Party, Opinion, OpinionREP
 from models import CountryMEP, GroupMEP, DelegationRole
-from models import Committee, Delegation, Country, Group, MEP, CommitteeRole, DelegationRole
+from models import Committee, Delegation, Country, Group, MEP, CommitteeRole, DelegationRole, Organization , OrganizationMEP
 from django.contrib import admin
 
 admin.site.register(Committee)
@@ -10,6 +10,8 @@ admin.site.register(DelegationRole)
 admin.site.register(Country)
 admin.site.register(Group)
 admin.site.register(Party)
+admin.site.register(Organization)
+admin.site.register(OrganizationMEP)
 
 class CountriesInline(admin.TabularInline):
     model = CountryMEP
@@ -27,10 +29,14 @@ class CommitteeInline(admin.TabularInline):
     model = CommitteeRole
     extra = 1
 
+class OrganizationMEPInline(admin.TabularInline):
+    model = OrganizationMEP
+    extra = 1
+
 class MEPAdmin(admin.ModelAdmin):
     search_fields = ['last_name']
     list_filter = ('active', 'countries', 'groups')
     list_display = ('last_name', 'first_name', 'active')
-    inlines = [CountriesInline, GroupInline, DelegationInline, CommitteeInline]
+    inlines = [CountriesInline, GroupInline, DelegationInline, CommitteeInline, OrganizationMEPInline]
 
 admin.site.register(MEP, MEPAdmin)
